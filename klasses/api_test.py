626e6ca1e8ab5247371b704fb4e466cf4ea93c72
Giovanni Di Milia <gdimilia@mit.edu>
2017-04-26 11:45:59 -0400

    Added integration for bootcamp admission

--- klasses/api_test.py
+++ klasses/api_test.py
@@ -14,6 +14,7 @@ from klasses.api import (
     serialize_user_klasses,
 )
 from klasses.bootcamp_admissions_client import BootcampAdmissionClient
+from klasses.conftest import patch_get_admissions
 from klasses.factories import KlassFactory, InstallmentFactory
 from klasses.serializers import InstallmentSerializer
 from profiles.factories import UserFactory
@@ -24,7 +25,7 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture()
-def setup_test_data():
+def test_data(mocker):
     """
     Sets up the data for all the tests in this module
     """
@@ -38,24 +39,26 @@ def setup_test_data():
     order = OrderFactory.create(user=user, status=Order.FULFILLED)
     LineFactory.create(order=order, klass_id=klass_paid.klass_id, price=627.34)
 
+    patch_get_admissions(mocker, user)
+
     return user, klass_paid, klass_not_paid
 
 
-def test_serialize_user_klass_bootclient_equivalent(setup_test_data):
+def test_serialize_user_klass_bootclient_equivalent(test_data):
     """
     Test for serialize_user_klass to verify that passing or not passing the bootcamp client is equivalent.
     """
-    user, klass_paid, _ = setup_test_data
+    user, klass_paid, _ = test_data
 
     assert serialize_user_klass(user, klass_paid) == serialize_user_klass(
         user, klass_paid, BootcampAdmissionClient(user.email))
 
 
-def test_serialize_user_klass_paid(setup_test_data):
+def test_serialize_user_klass_paid(test_data):
     """
     Test for serialize_user_klass for a paid klass
     """
-    user, klass_paid, _ = setup_test_data
+    user, klass_paid, _ = test_data
 
     expected_ret = {
         "klass_id": klass_paid.klass_id,
@@ -72,11 +75,11 @@ def test_serialize_user_klass_paid(setup_test_data):
     assert expected_ret == serialize_user_klass(user, klass_paid)
 
 
-def test_serialize_user_klass_not_paid(setup_test_data):
+def test_serialize_user_klass_not_paid(test_data):
     """
     Test for serialize_user_klass for a not paid klass
     """
-    user, _, klass_not_paid = setup_test_data
+    user, _, klass_not_paid = test_data
 
     expected_ret = {
         "klass_id": klass_not_paid.klass_id,
@@ -93,11 +96,11 @@ def test_serialize_user_klass_not_paid(setup_test_data):
     assert expected_ret == serialize_user_klass(user, klass_not_paid)
 
 
-def test_serialize_user_klasses(setup_test_data):
+def test_serialize_user_klasses(test_data):
     """
     Test for serialize_user_klasses in normal case
     """
-    user, klass_paid, klass_not_paid = setup_test_data
+    user, klass_paid, klass_not_paid = test_data
     expected_ret = [
         {
             "klass_id": klass_paid.klass_id,
@@ -127,11 +130,11 @@ def test_serialize_user_klasses(setup_test_data):
     assert sorted(expected_ret, key=lambda x: x['klass_id']) == serialize_user_klasses(user)
 
 
-def test_serialize_user_klasses_paid_not_payable(setup_test_data, mocker):
+def test_serialize_user_klasses_paid_not_payable(test_data, mocker):
     """
     Test for serialize_user_klasses in case the user is not eligible to pay but she has already paid for the klass
     """
-    user, klass_paid, klass_not_paid = setup_test_data
+    user, klass_paid, klass_not_paid = test_data
     mocker.patch(
         'klasses.bootcamp_admissions_client.BootcampAdmissionClient.payable_klasses_ids',
         new_callable=PropertyMock,
