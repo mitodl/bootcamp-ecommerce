75a4cf8c877813b4c83ea7622e71a86ec6ebf5dc
Amir Qayyum Khan <amir-qayyum-khan@users.noreply.github.com>
2017-10-14 12:50:52 +0500

    Added terms and conditions (#130)
    * Added terms and conditions
    
    * Update scss
    
    * Used default variable
    
    * textual changes

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     url(r'^$', index, name='bootcamp-index'),
     url(r'^pay/$', pay, name='pay'),
     url(r'^terms_of_service/$', TemplateView.as_view(template_name='bootcamp/tos.html'), name='bootcamp-tos'),
+    url(r'^terms_and_conditions/$', TemplateView.as_view(template_name='bootcamp/tac.html'), name='bootcamp-tac'),
     url(r'^status/', include('server_status.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url('', include('ecommerce.urls')),
