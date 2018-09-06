1e0ed9350c19207c4205a818fdf2d0cf058c3121
George Schneeloch <gschneel@mit.edu>
2018-09-06 10:26:30 -0400

    Remove IS_OSX from env.sh (#218)

--- webpack_dev_server.sh
+++ webpack_dev_server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ef -o pipefail
 
-# Define some environment variables to figure out where we are running
-source ./scripts/envs.sh
-
 WEBPACK_HOST='0.0.0.0'
 WEBPACK_PORT='8098'
 
@@ -13,12 +10,8 @@ WEBPACK_PORT='8098'
 #       the webpack server on the host machine rather than the container.
 # If neither of those are true, running this script is basically a no-op.
 
-if [[ "$IS_OSX" == "true" && "$INSIDE_CONTAINER" == "true" ]] ; then
-  echo -e "EXITING WEBPACK STARTUP SCRIPT\nOSX Users: The webpack dev server should be run on your host machine."
-else
-  if [[ "$1" == "--install" ]] ; then
-    yarn install --frozen-lockfile && echo "Finished yarn install"
-  fi
-  # Start the webpack dev server on the appropriate host and port
-  node ./hot-reload-dev-server.js --host "$WEBPACK_HOST" --port "$WEBPACK_PORT"
+if [[ "$1" == "--install" ]] ; then
+yarn install --frozen-lockfile && echo "Finished yarn install"
 fi
+# Start the webpack dev server on the appropriate host and port
+node ./hot-reload-dev-server.js --host "$WEBPACK_HOST" --port "$WEBPACK_PORT"
