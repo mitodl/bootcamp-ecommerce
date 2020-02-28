a6a8e45c8d7056fb0de152f563d5bcb8ca9fc65b
annagav <annagav@mit.edu>
2020-02-28 14:52:26 -0500

    remove validationin sync_all_users

--- smapply/tasks.py
+++ smapply/tasks.py
@@ -1,10 +1,12 @@
 """Tasks for smapply"""
+import logging
 from bootcamp.celery import app
 from hubspot.api import make_contact_sync_message
 from hubspot.tasks import sync_bulk_with_hubspot
-from smapply.serializers import UserSerializer
 from smapply.api import list_users, process_user, SMApplyTaskCache
 from profiles.models import Profile
+
+log = logging.getLogger(__name__)
 
 
 @app.task
@@ -20,11 +22,11 @@ def sync_all_users():
 
         profile = Profile.objects.filter(smapply_id=sma_user['id']).first()
         if not profile:
-            serializer = UserSerializer(data=sma_user)
-            if serializer.is_valid():
+            try:
                 user = process_user(sma_user)
-
                 profiles_to_sync.append(user.profile)
+            except:  # pylint: disable=bare-except
+                log.exception('Syncing profile for SMA user %s failed', sma_user['id'])
 
     if profiles_to_sync:
         task_cache = SMApplyTaskCache()
