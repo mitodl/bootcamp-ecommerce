42a5f56783f6ff4223f004c763729d75538b2da0
annagav <annagav@mit.edu>
2020-03-18 14:29:21 -0400

    settings

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -143,7 +143,6 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
 )
 
