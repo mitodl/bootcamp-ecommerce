fcdb91712a13f178a1e5e1f6109a26a4617794b8
George Schneeloch <gschneel@mit.edu>
2020-05-05 10:34:19 -0400

    Add logging

--- jobma/permissions.py
+++ jobma/permissions.py
@@ -1,7 +1,12 @@
 """jobma permission classes"""
+import logging
+
 from django.conf import settings
 
 from rest_framework.permissions import BasePermission
+
+
+log = logging.getLogger(__name__)
 
 
 class JobmaWebhookPermission(BasePermission):
@@ -11,5 +16,10 @@ class JobmaWebhookPermission(BasePermission):
         header = request.headers.get("authorization", "")
         if header.startswith("Token "):
             token = header[len("Token "):]
-            return token == settings.JOBMA_WEBHOOK_ACCESS_TOKEN
+            if token == settings.JOBMA_WEBHOOK_ACCESS_TOKEN:
+                return True
+            else:
+                log.error("Token found but did not match")
+                return False
+        log.error("No token found in authorization")
         return False
