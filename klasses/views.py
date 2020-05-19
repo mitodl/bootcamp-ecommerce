b6be82cfdd2af11f953e6c753ead0cdc2f14b74d
George Schneeloch <gschneel@mit.edu>
2020-05-19 11:18:42 -0400

    Check for presence of an application

--- klasses/views.py
+++ klasses/views.py
@@ -1,4 +1,5 @@
 """views for bootcamps"""
+from django.db.models import Q
 from django.db.models.functions import Now
 from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
@@ -17,5 +18,5 @@ class BootcampViewSet(ListModelMixin, GenericViewSet):
         """Make a queryset which optionally shows what runs are available for enrollment"""
         queryset = BootcampRun.objects.all().select_related("bootcamp")
         if self.request.query_params.get("available") == "true":
-            queryset = queryset.filter(start_date__lt=Now()).exclude(enrollments__user=self.request.user)
+            queryset = queryset.filter(start_date__lt=Now()).exclude(applications__user=self.request.user)
         return queryset.order_by("id")
--- klasses/views_test.py
+++ klasses/views_test.py
@@ -1,4 +1,5 @@
 """Views for bootcamps"""
+from collections import namedtuple
 from datetime import timedelta
 
 from django.urls import reverse
@@ -8,8 +9,8 @@ from rest_framework.status import (
     HTTP_403_FORBIDDEN,
 )
 
+from applications.factories import BootcampApplicationFactory
 from klasses.factories import BootcampRunFactory
-from klasses.models import BootcampRunEnrollment
 from klasses.serializers import BootcampRunSerializer
 from main.utils import now_in_utc
 
@@ -17,34 +18,36 @@ from main.utils import now_in_utc
 pytestmark = pytest.mark.django_db
 
 
+RunInfo = namedtuple("RunInfo", ["run", "unavailable_run", "submitted_application_run"])
+
+
 @pytest.fixture
 def runs(user):
     """Make some test runs"""
     run = BootcampRunFactory.create()
     unavailable_run = BootcampRunFactory.create(start_date=now_in_utc()+timedelta(days=1))
-    already_enrolled_run = BootcampRunFactory.create()
-    BootcampRunEnrollment.objects.create(user=user, bootcamp_run=already_enrolled_run)
+    submitted_application_run = BootcampRunFactory.create()
+    BootcampApplicationFactory.create(user=user, bootcamp_run=submitted_application_run)
 
-    return run, unavailable_run, already_enrolled_run
+    yield RunInfo(run=run, unavailable_run=unavailable_run, submitted_application_run=submitted_application_run)
 
 
 # pylint: disable=redefined-outer-name
 def test_bootcamp_runs(client, user, runs):
     """The REST API should output a list of serialized bootcamp runs"""
-    run, unavailable_run, already_enrolled_run = runs
     client.force_login(user)
 
     full_list_resp = client.get(reverse("bootcamp-runs-list"))
     assert full_list_resp.status_code == HTTP_200_OK
     assert full_list_resp.json() == BootcampRunSerializer(
         instance=[
-            run, unavailable_run, already_enrolled_run
+            runs.run, runs.unavailable_run, runs.submitted_application_run
         ], many=True
     ).data
 
     available_resp = client.get(f"{reverse('bootcamp-runs-list')}?available=true")
     assert available_resp.status_code == HTTP_200_OK
-    assert available_resp.json() == BootcampRunSerializer(instance=[run], many=True).data
+    assert available_resp.json() == BootcampRunSerializer(instance=[runs.run], many=True).data
 
 
 def test_bootcamp_runs_not_logged_in(client):
