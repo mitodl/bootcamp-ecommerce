d2d40907375ea9f2df86e61a97f6e0893ab4f9c4
Matt Bertrand <mattbert@mit.edu>
2020-06-01 12:37:03 -0400

    Remove profile urls

--- main/urls.py
+++ main/urls.py
@@ -69,8 +69,6 @@ urlpatterns = [
     path("account/inactive/", react, name="account-inactive"),
     path("account/confirm-email/", react, name="account-confirm-email-change"),
     path("account-settings/", react, name="account-settings"),
-    path("profile/", react, name="profile-view"),
-    path("profile/edit/", react, name="profile-edit"),
     path("applications/", react, name="applications"),
     path("drawer/", react, name="drawer"),
     # Wagtail
