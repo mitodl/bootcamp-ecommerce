13652eb089946d4aa77ea7becd02b3a7dcbfc9df
George Schneeloch <gschneel@mit.edu>
2020-06-26 09:39:30 -0400

    Fix import

--- applications/tasks.py
+++ applications/tasks.py
@@ -1,11 +1,12 @@
 """Tasks for applications"""
 from applications import api
-from applications.models import BootcampApplication
 from main.celery import app
 
 
 @app.task
 def create_and_send_applicant_letter(application_id, *, is_acceptance):
     """Create and send an applicant letter"""
+    from applications.models import BootcampApplication
+
     application = BootcampApplication.objects.get(id=application_id)
     api.create_and_send_applicant_letter(application, is_acceptance=is_acceptance)
