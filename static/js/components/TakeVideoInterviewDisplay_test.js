ba50d3a3e4cd064e3e1bdad5e31408f2f13b8d4e
George Schneeloch <gschneel@mit.edu>
2020-06-30 14:26:34 -0400

    Display interview token in take video interview drawer (#839)
    * Show interview token on drawer
    
    * Update static/js/components/TakeVideoInterviewDisplay.js
    
    Co-authored-by: Peter Pinch <pdpinch+github@hawklake.com>
    
    * Reformat
    
    * Fix test
    
    Co-authored-by: Peter Pinch <pdpinch+github@hawklake.com>

--- static/js/components/TakeVideoInterviewDisplay_test.js
+++ static/js/components/TakeVideoInterviewDisplay_test.js
@@ -43,4 +43,12 @@ describe("TakeVideoInterviewDisplay", () => {
       submission.take_interview_url
     )
   })
+
+  it("shows the interview token", async () => {
+    const { wrapper } = await renderPage()
+    assert.include(
+      wrapper.find(".take-video-interview").text(),
+      String(submission.interview_token)
+    )
+  })
 })
