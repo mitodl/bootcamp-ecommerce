9813f642bb91c91fba8e23a47b49a253a970fb60
Doof <mitx-devops@mit.edu>
2020-08-20 16:11:55 +0000

    Release 0.52.2

--- RELEASE.rst
+++ RELEASE.rst
@@ -1,6 +1,11 @@
 Release Notes
 =============
 
+Version 0.52.2
+--------------
+
+- add implementation for letter template page customized signatory details
+
 Version 0.52.1 (Released August 20, 2020)
 --------------
 
--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ from main.envs import get_string, get_bool, get_int, get_list, get_any
 from main.sentry import init_sentry
 
 
-VERSION = "0.52.1"
+VERSION = "0.52.2"
 
 ENVIRONMENT = get_string(
     "BOOTCAMP_ENVIRONMENT",
