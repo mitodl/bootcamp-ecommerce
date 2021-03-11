9840936e4c12a750b72117a41b5436bacc9686d9
Umar Asghar <umar.asghar@arbisoft.com>
2021-03-11 19:07:45 +0500

    import bootcamp alumni

---
+++ profiles/management/commands/import_alumni.py
@@ -0,0 +1,18 @@
+"""Import a alumni spreadsheet"""
+from django.core.management.base import BaseCommand
+
+from profiles.api import import_alumni
+
+
+class Command(BaseCommand):
+    """Import a csv of bootcamp alumni"""
+
+    help = "Import a csv of bootcamp alumni"
+
+    def add_arguments(self, parser):
+        """Handle arguments"""
+        parser.add_argument("csv_path", type=str, help="path to the csv")
+
+    def handle(self, *args, **options):
+        """Import a csv of bootcamp alumni"""
+        import_alumni(options["csv_path"])
