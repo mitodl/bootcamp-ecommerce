ebc80646f89a2a2ff9177d6476c341db06269956
Umar Asghar <umar.asghar@arbisoft.com>
2021-04-08 17:32:35 +0500

    Hide bootcamps with allow_skip_steps from users who are ineligible

--- klasses/views.py
+++ klasses/views.py
@@ -4,6 +4,8 @@ from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
+from applications.constants import AppStates
+from applications.models import BootcampApplication
 from klasses.serializers import BootcampRunSerializer
 from klasses.models import BootcampRun
 
@@ -16,9 +18,19 @@ class BootcampViewSet(ListModelMixin, GenericViewSet):
 
     def get_queryset(self):
         """Make a queryset which optionally shows what runs are available for enrollment"""
-        queryset = BootcampRun.objects.all().select_related("bootcamp")
+        user = self.request.user
+        complete_applications = BootcampApplication.objects.filter(
+            user=user, state=AppStates.COMPLETE.value
+        )
+        # if user is an alumni or have already bought a course, show them all the bootcamps
+        if user.profile.can_skip_application_steps or complete_applications.exists():
+            queryset = BootcampRun.objects.all().select_related("bootcamp")
+        else:
+            queryset = BootcampRun.objects.filter(
+                allows_skipped_steps=False
+            ).select_related("bootcamp")
         if self.request.query_params.get("available") == "true":
             queryset = queryset.filter(start_date__gt=Now()).exclude(
-                applications__user=self.request.user
+                applications__user=user
             )
         return queryset.order_by("id")
