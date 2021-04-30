c00ae81f4f0d635c512470a7557e26f05dae8fa2
Asad Iqbal <7334669+asadiqbal08@users.noreply.github.com>
2021-04-30 10:48:03 +0500

    Management command to retire users (#1205)
    * Retire use management command
    
    * Address Matt feedback
    
    * Fix the failure
    
    Co-authored-by: asadiqbal08 <asad.iqbal@arbisoft.com>

---
+++ profiles/management/commands/retire_users.py
@@ -0,0 +1,133 @@
+"""
+Retire user from BootCamp
+"""
+import logging
+from argparse import RawTextHelpFormatter
+from urllib.parse import urlparse
+
+from django.conf import settings
+from django.core.management import BaseCommand, CommandError
+from social_django.models import UserSocialAuth
+
+from user_util import user_util
+from jobma.constants import EXPIRED
+from jobma.models import Interview
+from profiles.api import fetch_users
+
+log = logging.getLogger(__name__)
+
+
+RETIRED_USER_SALTS = ["bootcamp-retired-email"]
+RETIRED_EMAIL_FMT = "retired_email_{}@retired." + "{}".format(
+    urlparse(settings.SITE_BASE_URL).netloc
+)
+
+
+class Command(BaseCommand):
+    """
+    Retire user from BootCamp
+    """
+
+    help = """
+Retire one or multiple users. Username or email can be used to identify a user.
+
+For single user use:\n
+`./manage.py retire_users --user=foo` or do \n
+`./manage.py retire_users -u foo` or do \n
+`./manage.py retire_users -u foo@email.com` \n
+
+For multiple users, add arg `--user` for each user i.e:\n
+`./manage.py retire_users --user=foo --user=bar --user=baz` or do \n
+`./manage.py retire_users --user=foo@email.com --user=bar@email.com --user=baz` or do \n
+`./manage.py retire_users -u foo -u bar -u baz`
+"""
+
+    def create_parser(self, prog_name, subcommand, **kwargs):
+        """
+        create parser to add new line in help text.
+        """
+        parser = super(Command, self).create_parser(prog_name, subcommand)
+        parser.formatter_class = RawTextHelpFormatter
+        return parser
+
+    def add_arguments(self, parser):
+        """create args"""
+        # pylint: disable=expression-not-assigned
+        parser.add_argument(
+            "-u",
+            "--user",
+            action="append",
+            default=[],
+            dest="users",
+            help="Single or multiple user name",
+        )
+
+    def get_retired_email(self, email):
+        """ Convert user email to retired email format. """
+        return user_util.get_retired_email(email, RETIRED_USER_SALTS, RETIRED_EMAIL_FMT)
+
+    def display_messages(self, message, log_messages, is_error=False):
+        """
+        Display error on console
+        Args:
+            message (str): message to display
+            is_error (bool): is error for styling
+            log_messages (list): Accumulated message
+        """
+        self.stdout.write(message, style_func=self.style.ERROR if is_error else None)
+        log_messages.append(message)
+
+    def handle(self, *args, **kwargs):  # pylint: disable=unused-argument
+        users = kwargs.get("users", [])
+        if not users:
+            # show error when no user selected.
+            raise CommandError(
+                "No user(s) provided. Please provide user(s) using -u or --user."
+            )
+
+        users = fetch_users(kwargs["users"])
+        for user in users:
+            log_messages = []
+
+            # retire user
+            self.display_messages(f"Retiring user {user}", log_messages)
+
+            if not user.is_active:
+                self.stdout.write(
+                    self.style.ERROR(
+                        "User: '{user}' is already deactivated in MIT BootCamp".format(
+                            user=user
+                        )
+                    )
+                )
+                continue
+
+            # mark user inactive
+            user.is_active = False
+
+            # Change user password & email
+            email = user.email
+            user.email = self.get_retired_email(user.email)
+            user.set_unusable_password()
+            user.save()
+
+            self.display_messages(
+                f"Email changed from '{email}' to '{user.email}' and password is not useable now",
+                log_messages,
+            )
+
+            # reset user social
+            auth_delete_count, _ = UserSocialAuth.objects.filter(user=user).delete()
+            self.display_messages(
+                f"For user {user}: {auth_delete_count} SocialAuth rows deleted",
+                log_messages,
+            )
+
+            Interview.objects.filter(applicant=user).update(status=EXPIRED)
+            self.display_messages(
+                f"For user {user}: Interview statuses are expired now.", log_messages
+            )
+
+            # finish
+            self.display_messages(f"User '{user}' is retired", log_messages)
+            log.info("\n".join(log_messages[1:]))
---
+++ profiles/management/commands/retire_users_test.py
@@ -0,0 +1,113 @@
+"""retire user test"""
+from datetime import timedelta
+
+import pytest
+from django.contrib.auth import get_user_model
+from social_django.models import UserSocialAuth
+
+from applications.factories import BootcampApplicationFactory
+
+from jobma.factories import InterviewFactory, JobFactory
+from profiles.factories import UserFactory, UserSocialAuthFactory
+from profiles.management.commands import retire_users
+
+from klasses.factories import BootcampRunFactory
+from main.utils import now_in_utc
+
+User = get_user_model()
+
+COMMAND = retire_users.Command()
+
+
+@pytest.mark.django_db
+def test_single_success():
+    """test retire_users command success with one user"""
+    test_username = "test_user"
+
+    user = UserFactory.create(username=test_username, is_active=True)
+    UserSocialAuthFactory.create(user=user, provider="edX")
+
+    assert user.is_active is True
+    assert "retired_email" not in user.email
+    assert UserSocialAuth.objects.filter(user=user).count() == 1
+
+    COMMAND.handle("retire_users", users=[test_username])
+
+    user.refresh_from_db()
+    assert user.is_active is False
+    assert "retired_email" in user.email
+    assert UserSocialAuth.objects.filter(user=user).count() == 0
+
+
+@pytest.mark.django_db
+def test_multiple_success():
+    """test retire_users command success with more than one user"""
+    test_usernames = ["foo", "bar", "baz"]
+
+    for username in test_usernames:
+        user = UserFactory.create(username=username, is_active=True)
+        UserSocialAuthFactory.create(user=user, provider="not_edx")
+
+        assert user.is_active is True
+        assert "retired_email" not in user.email
+        assert UserSocialAuth.objects.filter(user=user).count() == 1
+
+    COMMAND.handle("retire_users", users=test_usernames)
+
+    for user_name in test_usernames:
+        user = User.objects.get(username=user_name)
+        assert user.is_active is False
+        assert "retired_email" in user.email
+        assert UserSocialAuth.objects.filter(user=user).count() == 0
+
+
+@pytest.mark.django_db
+def test_retire_user_with_email():
+    """test retire_users command success with user email"""
+    test_email = "test@email.com"
+
+    user = UserFactory.create(email=test_email, is_active=True)
+    UserSocialAuthFactory.create(user=user, provider="edX")
+
+    assert user.is_active is True
+    assert "retired_email" not in user.email
+    assert UserSocialAuth.objects.filter(user=user).count() == 1
+
+    COMMAND.handle("retire_users", users=[test_email])
+
+    user.refresh_from_db()
+    assert user.is_active is False
+    assert "retired_email" in user.email
+    assert UserSocialAuth.objects.filter(user=user).count() == 0
+
+
+@pytest.mark.django_db
+def test_interview_applications_expired():
+    """test retire_users command change the status of interviews to expired"""
+    test_username = "test_user"
+
+    user = UserFactory.create(username=test_username, is_active=True)
+    UserSocialAuthFactory.create(user=user, provider="edX")
+
+    assert user.is_active is True
+    assert "retired_email" not in user.email
+    assert UserSocialAuth.objects.filter(user=user).count() == 1
+
+    now = now_in_utc()
+    run = BootcampRunFactory.create(start_date=(now + timedelta(days=10)))
+
+    bootcamp_app = BootcampApplicationFactory.create(
+        state="AWAITING_USER_SUBMISSIONS", bootcamp_run=run
+    )
+    interview = InterviewFactory.create(
+        job=JobFactory.create(run=bootcamp_app.bootcamp_run), applicant=user
+    )
+
+    COMMAND.handle("retire_users", users=[test_username])
+
+    user.refresh_from_db()
+    interview.refresh_from_db()
+    assert user.is_active is False
+    assert "retired_email" in user.email
+    assert UserSocialAuth.objects.filter(user=user).count() == 0
+    assert interview.status == "expired"
