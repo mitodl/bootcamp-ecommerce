faee42d00e914004080e6e4c844c4b05dc729805
Umar Asghar <umar.asghar@arbisoft.com>
2021-11-09 19:49:01 +0500

    Change references to Apply/Admissions to text entry fields in the CMS (#1308)
    Change references to Apply/Admissions to text entry fields in the CMS

--- cms/models.py
+++ cms/models.py
@@ -457,6 +457,15 @@ class AdmissionsSection(BootcampRunChildPage):
         related_name="+",
         help_text="Image to show at the top of this section, size must be at least 1900x650 pixels.",
     )
+    how_to_title = models.CharField(
+        max_length=255, default="How to Apply", help_text="How to title"
+    )
+    how_to_link_text = models.CharField(
+        max_length=255, default="Admissions", help_text="How to title link text"
+    )
+    apply_now_button_text = models.CharField(
+        max_length=255, default="Apply Now", help_text="Apply now button text"
+    )
     notes = models.CharField(
         max_length=255, help_text="Any notes to display, e.g. application deadline etc."
     )
@@ -483,6 +492,9 @@ class AdmissionsSection(BootcampRunChildPage):
 
     content_panels = [
         ImageChooserPanel("admissions_image"),
+        FieldPanel("how_to_title"),
+        FieldPanel("how_to_link_text"),
+        FieldPanel("apply_now_button_text"),
         FieldPanel("notes"),
         FieldPanel("details"),
         FieldPanel("bootcamp_format"),
