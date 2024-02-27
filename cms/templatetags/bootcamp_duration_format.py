71702f8aa73040fa3d2143aa1f27f8a927af5a6b
Muhammad Anas <muhammad.anas@arbisoft.com>
2024-02-27 10:56:01 +0500

    docs: added doc string

--- cms/templatetags/bootcamp_duration_format.py
+++ cms/templatetags/bootcamp_duration_format.py
@@ -4,6 +4,7 @@ register = template.Library()
 
 @register.simple_tag
 def bootcamp_duration_format(start, end):
+    """Returns the formatted date to be displayed on bootcamps cards"""
     if start.year == end.year and start.month == end.month:
         return f"{start.strftime('%B %d')} - {end.strftime('%d, %Y')}"
     elif start.year == end.year:
