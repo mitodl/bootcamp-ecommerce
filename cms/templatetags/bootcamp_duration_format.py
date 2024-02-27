6ca039792a8668cc3f39f10cae55722cf7054a2e
Muhammad Anas <muhammad.anas@arbisoft.com>
2024-02-27 10:40:36 +0500

    refactor: created custom django template tags for bootcamp dates format

---
+++ cms/templatetags/bootcamp_duration_format.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def bootcamp_duration_format(start, end):
+    if start.year == end.year and start.month == end.month:
+        return f"{start.strftime('%B %d')} - {end.strftime('%d, %Y')}"
+    elif start.year == end.year:
+        return f"{start.strftime('%B %d')} - {end.strftime('%B %d, %Y')}"
+    else:
+        return f"{start.strftime('%B %d, %Y')} - {end.strftime('%B %d, %Y')}"
