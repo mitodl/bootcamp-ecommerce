3e73187a1d822dc9b8e90e56a5cae7a50e0fc297
Muhammad Anas <88967643+Anas12091101@users.noreply.github.com>
2024-02-29 19:06:36 +0500

    fix: dates display on the bootcamp cards (#1487)
    * fix: dates display on the cards
    
    * fix: dates display on the cards
    
    * refactor: created custom django template tags for bootcamp dates format
    
    * docs: added doc string
    
    * style: formatted with black
    
    * test: added test for bootcamp_duration_format custom template tag
    
    * refactor: test code

--- cms/template_tags_test.py
+++ cms/template_tags_test.py
@@ -1,8 +1,12 @@
 """Tests for custom CMS templatetags"""
+
+from datetime import date
+
 import pytest
 from wagtail.images.views.serve import generate_signature
 from wagtail_factories import ImageFactory
 
+from cms.templatetags.bootcamp_duration_format import bootcamp_duration_format
 from cms.templatetags.image_version_url import image_version_url
 
 
@@ -20,3 +24,17 @@ def test_image_version_url():
         result_url
         == f"/images/{expected_signature}/{image_id}/{image_filter}/?v={file_hash}"
     )
+
+
+@pytest.mark.parametrize(
+    "start, end, expected_format",
+    [
+        [date(2024, 1, 15), date(2024, 1, 17), "January 15 - 17, 2024"],
+        [date(2024, 1, 15), date(2024, 2, 15), "January 15 - February 15, 2024"],
+        [date(2024, 1, 15), date(2025, 1, 15), "January 15, 2024 - January 15, 2025"],
+        [date(2024, 1, 15), date(2025, 2, 15), "January 15, 2024 - February 15, 2025"],
+    ],
+)
+def test_bootcamp_duration_format(start, end, expected_format):
+    """bootcamp_duration_format should format the duration correctly"""
+    assert bootcamp_duration_format(start, end) == expected_format
---
+++ cms/templatetags/bootcamp_duration_format.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def bootcamp_duration_format(start, end):
+    """Returns the formatted date to be displayed on bootcamps cards"""
+    if start.year == end.year and start.month == end.month:
+        return f"{start.strftime('%B %d')} - {end.strftime('%d, %Y')}"
+    elif start.year == end.year:
+        return f"{start.strftime('%B %d')} - {end.strftime('%B %d, %Y')}"
+    else:
+        return f"{start.strftime('%B %d, %Y')} - {end.strftime('%B %d, %Y')}"
