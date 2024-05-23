9e465bc6017cbe25669c21e09fcdfe759f8d2459
Arslan <arslan.ashraf@arbisoft.com>
2024-05-23 15:20:49 +0500

    port over migration detection scripts from xPRO

--- scripts/test/detect_missing_migrations.sh
+++ scripts/test/detect_missing_migrations.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-TMPFILE=$(mktemp)
+MIGRATIONS_OUTPUT=
+
 fail() {
-    echo "Error: one or more migrations are missing"
-    echo
-    cat "$TMPFILE"
-    rm "$TMPFILE"
-    exit 1
+	echo "Error: one or more migrations are missing"
+	echo $MIGRATIONS_OUTPUT
+	exit 1
 }
 
-./manage.py makemigrations --no-input --dry-run >& "$TMPFILE"
-if [[ $? -ne 0 ]]
-then
-    # makemigrations has returned a non-zero for some reason, possibly
-    # because it needs input but --no-input is set
-    fail
-elif [[ $(cat "$TMPFILE" | grep "No changes detected" | wc -l) -eq 0 ]]
-then
-    fail
-else
-    rm "$TMPFILE"
+MIGRATIONS_OUTPUT="$(poetry run python ./manage.py makemigrations --no-input --dry-run 2>&1)"
+if [[ $? -ne 0 ]]; then
+	# makemigrations has returned a non-zero for some reason, possibly
+	# because it needs input but --no-input is set
+	fail
+elif [[ $(echo "$MIGRATIONS_OUTPUT" | grep "No changes detected" | wc -l) -eq 0 ]]; then
+	fail
 fi
